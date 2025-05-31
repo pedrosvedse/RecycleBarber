bf0abef1067dbc305fb16af5268817fe100469bb
Rebeca Lyra <arthur.ferreira0020016111@edu.pe.senac.br>
2025-05-31 10:52:13 -0300

    Lista de tarefas
    Lista do que fazer no coleta.py

--- coleta.py
+++ coleta.py
@@ -1,1 +1,16 @@
-print()
+# A FAZER:
+
+# criar classe de solicitação de coleta
+#- ID da solicitação
+#- barbearia que solicitou
+#- id do parceiro
+#- data da solictiação
+#- tipos de resíduo
+#- status da coleta
+#- data prevista para a colete
+
+# funções para solicitação:
+#- solicitar coleta (cria um novo objeto de solicitação de coleta)
+#- acompanhar coleta
+#- finalizar coleta
+#- cancelar coleta
