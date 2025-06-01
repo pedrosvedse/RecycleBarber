503f8144f039a86a25cd4a760023adfd0fc8c524
pedrosvedse <pg520444@gmail.com>
2025-06-01 20:22:21 -0300

    Update coleta.py
    Tentar finalizar uma coleta que já foi finalizada (ou não está mais em 'ativas')

--- coleta.py
+++ coleta.py
@@ -210,3 +210,6 @@ print("\n--- Passo 1: Solicitação de Coletas ---")
     print("\n--- Passo 5: Finalizar Coletas ---")
     if coleta_barbeiro1_a:
         finalizar_coleta(coleta_barbeiro1_a.id_coleta, "Material recolhido pelo coletor Equipe Alpha.")
+        
+if coleta_barbeiro1_a:
+        finalizar_coleta(coleta_barbeiro1_a.id_coleta)
